57ebd081c0b7fd52059c8979f9e86d424bb6aaf5
selen <slnkprmci01@gmail.com>
2023-12-29 00:32:15 +0300

    second for project  user-service

---
+++ foodApp/foodApp/user-service/src/main/java/com/FoodOrderingApp/model/UserDTO.java
@@ -0,0 +1,16 @@
+package com.FoodOrderingApp.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+    private String username;
+    private String email;
+}
---
+++ foodApp/foodApp/user-service/src/main/java/com/FoodOrderingApp/service/UserService.java
@@ -0,0 +1,56 @@
+package com.FoodOrderingApp.service;
+
+import com.FoodOrderingApp.model.User;
+import com.FoodOrderingApp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    // UserRepository enjekte edilir
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    // Yeni kullanıcı oluşturur
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    // ID'ye göre kullanıcı getirir
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    // Tüm kullanıcıları listeler
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    // Kullanıcıyı günceller
+    public User updateUser(Long id, User userDetails) {
+        return userRepository.findById(id)
+                .map(user -> {
+                    user.setUsername(userDetails.getUsername());
+                    user.setEmail(userDetails.getEmail());
+                    // Diğer alanları güncelle
+                    return userRepository.save(user);
+                })
+                .orElseGet(() -> {
+                    userDetails.setId(id);
+                    return userRepository.save(userDetails);
+                });
+    }
+
+    // Kullanıcıyı siler
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+}
