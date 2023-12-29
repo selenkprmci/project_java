79e39963d979410f9208bbe5d6caf22ae380154c
selen <slnkprmci01@gmail.com>
2023-12-29 18:34:14 +0300

    commit for user-service.

--- foodApp/foodApp/user-service/src/main/java/com/FoodOrderingApp/model/User.java
+++ foodApp/foodApp/user-service/src/main/java/com/FoodOrderingApp/model/User.java
@@ -1,15 +1,15 @@
 package com.FoodOrderingApp.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.annotation.Id;
-
-
 
 @Entity
 @Getter
@@ -18,13 +18,16 @@ import org.springframework.data.annotation.Id;
 @NoArgsConstructor
 public class User {
 
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
 
     private String username;
     private String password;
     private String email;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
 
 }
