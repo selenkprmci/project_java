27473b3ada0794cf59a8f74adc9e2cf343259b30
selen <slnkprmci01@gmail.com>
2023-12-29 15:01:58 +0300

    Changes by alp_c

--- foodApp/foodApp/user-service/src/main/java/com/FoodOrderingApp/Main.java
+++ foodApp/foodApp/user-service/src/main/java/com/FoodOrderingApp/Main.java
@@ -1,7 +1,19 @@
 package com.FoodOrderingApp;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(Main.class, args);
+        System.out.println("selenperen");
+    }
+
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
     }
 }
