c2182f79d8af748dc6cf7bc30116b1f72c5ef1b0
selen <slnkprmci01@gmail.com>
2023-12-29 23:02:34 +0300

    commit with html file

--- foodApp/foodApp/user-service/src/main/java/com/FoodOrderingApp/controller/UserController.java
+++ foodApp/foodApp/user-service/src/main/java/com/FoodOrderingApp/controller/UserController.java
@@ -4,12 +4,13 @@ import com.FoodOrderingApp.model.User;
 import com.FoodOrderingApp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.ui.Model;
 import java.util.List;
 import java.util.Optional;
 
-@RestController
+@Controller
 @RequestMapping("/api/users")
 public class UserController {
 
@@ -22,25 +23,33 @@ public class UserController {
     }
 
     // Yeni kullanıcı oluşturma endpoint'i
+
     @PostMapping("/register")
-    public ResponseEntity<User> registerUser(@RequestBody User newUser) {
-        User user = userService.createUser(newUser);
-        return ResponseEntity.ok(user);
+    public String registerUser(User user) {
+        userService.createUser(user);
+        return "redirect:/api/users"; // Başarılı kayıttan sonra kullanıcıları listeleme sayfasına yönlendir
+    }
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("user", new User());
+        return "register";
     }
 
     // Kullanıcı bilgilerini ID'ye göre getirme endpoint'i
-    @GetMapping("/{userId}")
+    @GetMapping("/profile/{userId}")
     public ResponseEntity<Optional<User>> getUserProfile(@PathVariable Long userId) {
         Optional<User> user = userService.getUserById(userId);
-        return ResponseEntity.ok(user);
+        return ResponseEntity.ok(user); // Kullanıcı profilini döndür
     }
 
     // Tüm kullanıcıları getirme endpoint'i
     @GetMapping
-    public ResponseEntity<List<User>> getAllUsers() {
+    public String getAllUsers(Model model) {
         List<User> users = userService.getAllUsers();
-        return ResponseEntity.ok(users);
+        model.addAttribute("users", users);
+        return "users"; // templates dizinindeki 'users.html' şablonunu kullanır
     }
+
 
     // Kullanıcı güncelleme endpoint'i
     // Burada daha fazla detay eklenebilir (örneğin, güncellenecek alanlar)
